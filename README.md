c9be15fd3f1b182644e57d0badffd63e9597e3dd
Shean Lin <shean.lin2018@gmail.com>
2020-06-01 18:22:53 -0500

    behold.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# hard-worker
-Push commits later. Or really early. Whatever. 
+# Hard-Worker
+Stay late! Get up early! Without having to stay late or get up early!
+
+As easy as `hardwork`.
+
+### What?
+This script `git pushes` your changes for you automatically, really late in the evening, or early in the morning, or whenever you want. Uses:
+1. Looking like a hard worker
+2. Looking like an early riser
+3. Slowly pushing several changes over the course of a week while you secretly road trip across the country
+
+### Install
+1. `curl https://raw.githubusercontent.com/slin63/hard-worker/master/README.md -o /usr/local/bin/hardworker`
+1. Test if it's working by starting the hardwork process with `hardwork -s`
+1. You may have to change `/usr/local/bin` to some other valid directory in your `$PATH`.
+
+#### Start the hardwork process
+`$ hardwork -s`
+
+#### Enqueue the current repo to be pushed later at a random time (after 5PM local time, of course)
+`$ hardwork`
+
+#### Enqueue the current repo to be pushed later after a specified delay time
+`$ hardwork -d 3 # Will push in 3 hours`
+
+
+```
+hardwork -d <hours> -s -h (--help)
+    -d how many hours from now to git push the current git repo
+    -s run the hardwork process, necessary for hardwork to... work
+    -h / --help: print out this message
+```
---
+++ hard-worker.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+from typing import Dict, List, Tuple
+
+import random
+import argparse
+import os
+import subprocess
+
+from time import sleep
+
+from datetime import datetime
+from datetime import timedelta
+
+WATCH_INTERVAL = 1
+QUEUE = "/tmp/hardworkq.txt"
+random.seed()
+
+
+def start_process():
+    """Watch QUEUE for jobs to do."""
+    jobs_finished: Dict[str, Tuple[datetime, bool]] = {}
+    while True:
+        jobs: Dict[str, datetime] = {}
+        pushed = False
+        with open(QUEUE, "r") as queue:
+            for job in queue.readlines():
+                repo, push_time = job.strip().split("!!")
+                push_time = datetime.strptime(
+                    push_time, "%Y-%m-%d %H:%M:%S.%f"
+                )
+
+                # Check if this job should be executed
+                if datetime.now() >= push_time:
+                    pushed = True
+                    print(f"Pushing {'...' + repo[-20:]}!")
+                    success = git_push(repo)
+                    jobs_finished[repo] = (push_time, success)
+                else:
+                    jobs[repo] = push_time
+
+        # If we completed a job, regardless of success, update the queue
+        if pushed:
+            with open(QUEUE, "w") as queue:
+                for repo, push_time in jobs.items():
+                    queue.write(f"{repo}!!{push_time}\n")
+
+        sleep(WATCH_INTERVAL)
+        print("\n---")
+        print(render_jobs(jobs))
+        print(render_jobs_finished(jobs_finished))
+        print("---")
+
+
+def render_jobs(jobs: Dict[str, datetime]) -> str:
+    out = "Hard work to be done:\n"
+    for repo, push_time in jobs.items():
+        out += f"  • {'...' + repo[-20:]}: {push_time.strftime('[%I:%M %p]')}\n"
+
+    return out.strip()
+
+
+def render_jobs_finished(
+    jobs_finished: Dict[str, Tuple[datetime, bool]]
+) -> str:
+    out = "Hard work that has been done:\n"
+    for repo, results in jobs_finished.items():
+        push_time, success = results
+        if success:
+            success_str = "✓ successful!"
+        else:
+            success_str = "⨯ failed!"
+        out += f"  • {'...' + repo[-20:]}: {success_str} {push_time.strftime('[%I:%M %p]')}\n"
+
+    return out.strip()
+
+
+def git_push(repo: str) -> bool:
+    # p = subprocess.Popen(["git", "push"], cwd=repo)
+    p = subprocess.Popen(
+        ["touch", f"hardworkertest{datetime.now()}"], cwd=repo
+    )
+    p.wait()
+
+    return p.returncode == 0
+
+
+def add_to_queue(repo: str, push_time: datetime) -> None:
+    with open(QUEUE, "a") as queue:
+        queue.write(f"{repo}!!{push_time}\n")
+
+
+def get_current_repo(current_path: str) -> str:
+    """Check if the given path is part of a valid repo"""
+    repo = subprocess.Popen(
+        "git rev-parse --show-toplevel",
+        shell=True,
+        stdout=subprocess.PIPE,
+    )
+    stdout, _ = repo.communicate()
+
+    return stdout.decode("utf-8").strip()
+
+
+def get_push_time(delay: int) -> datetime:
+    # If a delay is not specified, push anywhere from
+    # 2 - 5 hours after 5PM local.
+    if not delay:
+        end_of_day = datetime.now().replace(hour=17)
+        # return datetime.now() + timedelta(
+        #     hours=random.randint(2, 5), minutes=random.randint(0, 60),
+        # )
+        return datetime.now() + timedelta(
+            minutes=random.randint(0, 1),
+        )
+    # Otherwise, return the current time + <delay> hours
+    return datetime.now() + timedelta(hours=delay)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Let's get some work done."
+    )
+    parser.add_argument(
+        "-d",
+        "--delay",
+        type=int,
+        help="number of hours to delay a push",
+    )
+    parser.add_argument(
+        "-p",
+        "--process",
+        action="store_true",
+        help="run the hardwork process",
+    )
+
+    args = parser.parse_args()
+    current_path = os.getcwd()
+    print(args.delay, args.process)
+
+    # Add this to the queue.
+    if not args.process:
+        push_time = get_push_time(args.delay)
+        repo_root = get_current_repo(current_path)
+        if not repo_root:
+            raise Exception("Not a valid git repo.")
+
+        add_to_queue(repo_root, push_time)
+        time_till_push = (
+            push_time - datetime.now()
+        ).total_seconds() / 3600
+        print(
+            f"{'...' + repo_root[-20:]} set to push in {time_till_push:.0f} hours! Nice work."
+        )
+    # Otherwise, start the process.
+    else:
+        start_process()
