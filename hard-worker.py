856c927492fe6fd30df7b78902614692af354eb2
Shean Lin <shean.lin2018@gmail.com>
2020-06-01 20:51:56 -0500

    remove comments

--- hard-worker.py
+++ hard-worker.py
@@ -67,11 +67,8 @@ def start_process():
 
 
 def clear():
-    # for windows
     if os.name == "nt":
         os.system("cls")
-
-    # for mac and linux(here, os.name is 'posix')
     else:
         os.system("clear")
 
